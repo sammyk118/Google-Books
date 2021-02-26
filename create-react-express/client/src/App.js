5edd6441ed9de77c4c4e1b71a9699174962edb28
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-26 11:54:32 -0800

    Create Table component

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import NavBar from "./Components/NavBar";
 import Jumbotron from "./Components/Jumbotron";
 import SearchBar from "./Components/SearchBar"
+import Table from "./Components/Table"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Jumbotron />
       <SearchBar/>
+      <Table/>
     </div>
   );
 }
---
+++ create-react-express/client/src/Components/Table/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function Table() {
+    return(
+  <div className="card">
+    <h5 className="card-header">Results</h5>
+    
+    <div className="card-body">
+      <h5 className="card-title">Book Title</h5>
+      <p className="card-text">
+        Description
+      </p>
+  
+    </div>
+  </div>
+    )
+}
+
+export default Table;
