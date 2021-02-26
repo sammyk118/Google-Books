0012af5ca94e2f0def28a254e0ed5453db59b7f4
Sammy Kroner <sammyk118@gmail.com>
2021-02-26 11:57:19 -0800

    added most routes

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div>
+        <Switch>
+          {/* <Route exact path="/books" component={books} />
+          <Route exact path="/search" component={search} /> */}
+          <Route exact path="*" />
+        </Switch>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
---
+++ create-react-express/routes/api/books.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.create);
+
+router.route("/")
+  .post(booksController.create)
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+
+
+module.exports = router;
