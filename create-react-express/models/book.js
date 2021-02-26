e072b339c7014668f2a989c23df4f4a359fc45e9
Sammy Kroner <sammyk118@gmail.com>
2021-02-26 11:11:16 -0800

    1st push

---
+++ create-react-express/models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: { type: String },
+    authors: { type: String },
+    description: { type: String },
+    image: { type: String },
+    link: { type: String }
+})
+
+const Book = mongoose.model("Book", bookSchema)
+
+module.exports = Book
