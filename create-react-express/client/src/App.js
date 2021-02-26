cacdce66eb158f2fe1b0f26d456362720b1dd98b
Rand Hale <randluca@gmail.com>
2021-02-26 14:22:47 -0800

    progress with routes and logic in components

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from "react";
+import React, { component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import NavBar from "./Components/NavBar";
-import Jumbotron from "./Components/Jumbotron";
-import SearchBar from "./Components/SearchBar"
-import Table from "./Components/Table"
+// import NavBar from "./Components/NavBar";
+// import Jumbotron from "./Components/Jumbotron";
+// import SearchBar from "./Components/SearchBar";
+// import Table from "./Components/Table";
+import Saved from "./Components/Saved";
+import Search from "./Components/Search";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Jumbotron />
-      <SearchBar/>
-      <Table/>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Search} />
+        <Route path="/saved" component={Saved} />
+      </Switch>
+    </Router>
   );
 }
 
--- create-react-express/client/src/Components/SearchBar/index.js
+++ create-react-express/client/src/Components/SearchBar/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./style.css";
 
-function SearchBar() {
+
+function SearchBar(props) {
   return (
     <div className="card">
-      <div class="input-group">
+      <div className="input-group">
         <input
           type="search"
-          class="form-control rounded"
+          className="form-control rounded"
           placeholder="Search"
           aria-label="Search"
           aria-describedby="search-addon"
+          value={props.value}
+          onChange={props.onChange}
         />
-        <button type="button" class="btn btn-outline-primary">
+        <button type="button" className="btn btn-outline-primary" onClick={props.onClick} >
           Search
         </button>
       </div>
