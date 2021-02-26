c8b5e16d4def451bff03dbee8be97e17d0fb5450
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-02-26 11:44:34 -0800

    Create SearchBar component

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import Jumbotron from "./Components/Jumbotron";
+import SearchBar from "./Components/SearchBar"
 
 function App() {
   return (
     <div>
       <NavBar />
       <Jumbotron />
+      <SearchBar/>
     </div>
   );
 }
